4171b8ec50097815b06880c495c9010baeb2a7d9
Darshil Shah <darshil.saphcm@gmail.com>
2018-03-18 22:03:46 +0530

    Measure Screen with Java

--- app/src/main/java/com/example/x_men/firstfragment/MainActivity.java
+++ app/src/main/java/com/example/x_men/firstfragment/MainActivity.java
@@ -4,42 +4,62 @@ import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 //    private SimpleFragment fragment;
-    public static final String FRAGMENT_TAG = "fragment_tag";
+//    public static final String FRAGMENT_TAG = "fragment_tag";
+    private TextView mOut;
+    private boolean mUseFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        mOut = (TextView) findViewById (R.id.text_out);
 
 //        fragment = new SimpleFragment ();
 
     }
 
-    public void clickHandler(View view) {
+    public void onMeasureBtnClick(View view){
+        ScreenUtility su = new ScreenUtility (this);
 
+        mOut.setText (String.format ("Width: %s, Height: %s", su.getDpWidth (), su.getDpHeight ()));
 
-        SimpleFragment fragment = SimpleFragment.newInstance ("Passed to factory method");
-        getSupportFragmentManager ()
-                .beginTransaction ()
-                .addToBackStack (null) //To Remove using Back Button
-                .add(R.id.fragment_container, fragment, FRAGMENT_TAG)
-                .commit ();
-    }
-
-    public void removeClickHandler(View view) {
-
-//        android.support.v4.app.Fragment fragment = getSupportFragmentManager ().findFragmentById (R.id.fragment_container);
-        android.support.v4.app.Fragment fragment = getSupportFragmentManager ().findFragmentByTag (FRAGMENT_TAG);
-        if (fragment != null) {
-            getSupportFragmentManager ()
-                    .beginTransaction ()
-                    .remove (fragment)
-                    .commit ();
+        if(su.getDpWidth () >= 640){
+            mUseFragment = true;
+        } else{
+            mUseFragment = false;
         }
+
+        Toast.makeText (this, "Using Fragment ? " + mUseFragment, Toast.LENGTH_SHORT).show ();
+
     }
+
+//    public void clickHandler(View view) {
+//
+//
+////        SimpleFragment fragment = SimpleFragment.newInstance ("Passed to factory method");
+////        getSupportFragmentManager ()
+////                .beginTransaction ()
+////                .addToBackStack (null) //To Remove using Back Button
+////                .add(R.id.fragment_container, fragment, FRAGMENT_TAG)
+////                .commit ();
+//    }
+//
+//    public void removeClickHandler(View view) {
+//
+////        android.support.v4.app.Fragment fragment = getSupportFragmentManager ().findFragmentById (R.id.fragment_container);
+//        android.support.v4.app.Fragment fragment = getSupportFragmentManager ().findFragmentByTag (FRAGMENT_TAG);
+//        if (fragment != null) {
+//            getSupportFragmentManager ()
+//                    .beginTransaction ()
+//                    .remove (fragment)
+//                    .commit ();
+//        }
+//    }
 }
