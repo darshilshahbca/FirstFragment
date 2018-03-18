d076848c9f4cca05850eaaae3c5f00c26ee6048d
Darshil Shah <darshil.saphcm@gmail.com>
2018-03-18 20:38:22 +0530

    Add Fragment with Java

--- app/src/main/java/com/example/x_men/firstfragment/MainActivity.java
+++ app/src/main/java/com/example/x_men/firstfragment/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.x_men.firstfragment;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SimpleFragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        fragment = new SimpleFragment ();
+
+    }
+
+    public void clickHandler(View view) {
+        getSupportFragmentManager ()
+                .beginTransaction ()
+                .add(R.id.fragment_container, fragment)
+                .commit ();
     }
 }
